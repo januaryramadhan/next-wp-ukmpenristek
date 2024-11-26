7a1c1ffd1e3884ae2b914873c060f95d477f5390
january <044777355@ecampus.ut.ac.id>
2024-11-27 03:45:03 +0700

    Remove 'KegiatanMingguIni' section from the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,11 @@ import FAQ from "@/components/sections/faq";
 import VisiMisi from "@/components/sections/visi-misi";
 import { StrukturOrganisasi } from "@/components/charts/strukturOrganisasi";
 import { NewestPost } from "@/components/sections/newest-post";
-import KegiatanMingguIni from "@/components/sections/upcoming event";
 
 export default async function Home() {
   return (
     <>
       <Hero1 />
-      <KegiatanMingguIni />
       <NewestPost />
       <VisiMisi />
       <StrukturOrganisasi />
